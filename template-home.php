126b2ee4587e14c3c2eaf1f70af439d71cdca274
Kyle Crookshank <kyle@crookshanked.com>
2015-03-29 22:02:49 -0400

    Initial repo start. sass/html5blank

---
+++ template-home.php
@@ -0,0 +1,41 @@
+<?php /* Template Name: Home Page Template */ get_header(); ?>
+
+	<main class="main" role="main">
+		<!-- section -->
+		<section class="main-section">
+
+			<h1 class="page-title"><?php the_title(); ?></h1>
+
+		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+			<!-- article -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				
+				<div class="page-content">
+					<?php the_content(); ?>
+				</div>
+		
+			</article>
+			<!-- /article -->
+
+		<?php endwhile; ?>
+
+		<?php else: ?>
+
+			<!-- article -->
+			<article>
+
+				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+
+			</article>
+			<!-- /article -->
+
+		<?php endif; ?>
+
+		</section>
+		<!-- /section -->
+	</main>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
